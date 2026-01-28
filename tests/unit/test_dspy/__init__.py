a154f7be9d32d1424154f4c24020eacfe58ca6b4
Avngrstark62 <thakurabhijeetsingh79@gmail.com>
2026-01-28 07:56:48 +0530

    cleanup

--- tests/unit/test_dspy/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Unit tests for DSPy runtime components.
-
-This package contains unit tests for the continuous/online path of the DSPy integration:
-- Prompt management and selection
-- Dataset pipeline
-- Interaction extraction
-"""
